d25a4f5f82cb5321b74bc861dca4701f182cfc4e
huitaek <rkfgus90@naver.com>
2019-06-06 16:05:42 +0900

    Basic Variables, Data Types

---
+++ 1_Day/Exp_04/Exp_04/Exp_04-2.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+
+#include <stdio.h>
+
+int main() { 
+	int a = 2; // 초기화 (선언과 동시에 대입)
+	int b = 3;
+
+	printf("%d + %d = %d\n", a, b, a + b);
+
+	// 변수 이름 : 알파벳 대소문자, _, 숫자(첫번째 글자) - int 2a; (X)
+
+}
---
+++ 1_Day/Exp_04/Exp_04/Exp_04-3.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+
+#include <stdio.h>
+
+int main() {
+	int a = 2;
+	int b = 3;
+	int hap = a + b;
+
+	printf("%d + %d = %d\n", a, b, hap);
+}
