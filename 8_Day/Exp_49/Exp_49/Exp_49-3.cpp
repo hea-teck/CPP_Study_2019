dbd6ce8273d9a4f65842cd88883cf6eddb12a6c5
hea-teck <rkfgus90@naver.com>
2019-08-10 17:31:28 +0900

    Basic I/O

---
+++ 8_Day/Exp_49/Exp_49/Exp_49-3.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include "iostream"
+#include "string"
+
+using namespace std;
+
+int main() {
+	string str;
+	str = "Hello";
+	cout << str << endl;
+}
---
+++ 8_Day/Exp_49/Exp_49/Exp_49-4.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "iostream"
+#include "string"
+
+using namespace std;
+
+int main() {
+	string name;
+
+	cout << "¿Ã∏ß ¿‘∑¬ : ";
+	cin >> name;
+
+	string message = "æ»≥Á«œººø‰, " + name + " ¥‘.";
+	cout << message << endl;
+}
